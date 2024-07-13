9afb1b8052d3dc74a521685890273c9cce97f984
Muneeb Ali <146629529+Muneeb00alii@users.noreply.github.com>
2024-07-13 11:46:15 +0500

    Create packages.py

---
+++ packages.py
@@ -0,0 +1,55 @@
+
+#! Jokes List 
+jokes_list = [
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "What do you call a fish wearing a crown? An anemone!",
+    "Why did the tomato turn red? Because it saw the salad dressing!",
+    "What do you call a belt made out of watches? A waist of time!",
+    "Why did the math book look sad? Because it had too many problems!",
+    "What do you call a bear with no teeth? A gummy bear!",
+    "Why did the golfer bring two pairs of pants? In case he got a hole in one!",
+    "Why did the computer go to the doctor? Because it had a virus!",
+    "Why was the math book sad? Because it had too many problems!",
+    "Why did the tomato turn red? Because it saw the salad dressing!",
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "What do you call a fake noodle? An impasta!",
+    "Why did the bicycle fall over? Because it was two-tired!",
+    "What do you call a pile of cats? A meowtain!"
+]
+
+#! Misic Dictionary
+music_dict = {
+    # Arijit Singh Songs
+    "tum hi ho": "https://www.youtube.com/watch?v=Umqb9KENgmk",
+    "channa mereya": "https://www.youtube.com/watch?v=284Ov7ysmfA",
+    "kabira": "https://www.youtube.com/watch?v=jHNNMj5bNQw",
+    "tera yaar hoon main": "https://www.youtube.com/watch?v=EatzcaVJRMs",
+    "ae dil hai mushkil": "https://www.youtube.com/watch?v=6FURuLYrR_Q",
+    "khairiyat": "https://www.youtube.com/watch?v=hoNb6HuNmU0",
+    "hawayein": "https://www.youtube.com/watch?v=cYOB941gyXI",
+    "dua karo": "https://www.youtube.com/watch?v=3mBpgxzAn6E",
+    
+    # Atif Aslam Songs
+    "tera hone laga hoon": "https://www.youtube.com/watch?v=useznoRhrWU",
+    "dil diyan gallan": "https://www.youtube.com/watch?v=SAcpESN_Fk4",
+    "tu jaane na": "https://www.youtube.com/watch?v=P8PWN1OmZOA",
+
+    # Diljit Dosanjh Songs
+    "do you know": "https://www.youtube.com/watch?v=P-DhwN87JDY",
+    "high end": "https://www.youtube.com/watch?v=UCoogBnurWk",
+    "goat": "https://www.youtube.com/watch?v=cl0a3i2wFcc",
+    "born to shine": "https://www.youtube.com/watch?v=dCmp56tSSmA",
+
+    # Attaullah Khan Esakhelvi Songs
+    "idhar zindagi ka janaza": "https://www.youtube.com/watch?v=hqGkNMGCres",
+    "tum ko shohrat ho mubarak": "https://www.youtube.com/watch?v=k-_rGb2Fyuo",
+    "pyar naal na sahi": "https://www.youtube.com/watch?v=oRoDbwoeBcQ",
+
+    # Old Bollywood Songs
+    "kar na sake hum pyar ka sauda": "https://www.youtube.com/watch?v=iv6brSMETyY",
+    "shikwa nahi kisi": "https://www.youtube.com/watch?v=fSan-Mv4AnI",
+    "tum ne meri zindagi": "https://www.youtube.com/watch?v=zJK6neVXzeE",
+    "barsaat ke mausam mein": "https://www.youtube.com/watch?v=rrzSZ0NMID4"
+
+}
